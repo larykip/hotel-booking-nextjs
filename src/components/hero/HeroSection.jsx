1af7462cf5963b2dffa739523f7a41bf02668d81
mergik <122933591+mergik@users.noreply.github.com>
2024-08-25 18:21:45 +0300

    chore: Fixed hero image responsive sizing

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
       {/* Hero Image */}
       <div className='basis-1/2 relative w-[500px] h-[500px] '>
       <Image 
-        className='rounded-lg' 
+        className='rounded-lg object-cover' 
         src='https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
         fill 
         alt='hero'
