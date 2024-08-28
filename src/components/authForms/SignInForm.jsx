fbb4ed5ccb04a4bfad7ad89e30572b57ec047d2e
mergik <122933591+mergik@users.noreply.github.com>
2024-08-28 19:55:36 +0300

    Rebuilt signin form

---
+++ src/components/authForms/SignInForm.jsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+// sign in form schema using zod
+const formSchema = z.object({
+    emailAddress: z.string().email(),
+    password: z.string().min(6),
+});
+
+const SignInForm = () => {
+    // form definition
+    const form = useForm({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            emailAddress: "",
+            password: "",
+        }
+    });
+
+    // our submit handler
+    const onSubmit = (values) => {
+        console.log({values});
+    }
+
+    return (
+        <main className='flex flex-col min-h-full items-center justify-between my-5 '>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col max-w-full w-full gap-5">
+                    {/* Start of Email address field */}
+                    <FormField
+                        control={form.control}
+                        name="emailAddress"
+                        render={({field}) => {
+                            return <FormItem>
+                                <FormLabel>Email Address</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Email address" type="email" {...field}/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        }}
+                    />
+                    {/* End of Email address field */}
+
+                    {/* Start of password field */}
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({field}) => {
+                            return <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Password" type="password" {...field}/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        }}
+                    />
+                    {/* End of password field */}
+
+                    <Button type="submit" className="w-full">Sign in with Email</Button>
+                </form>
+            </Form>
+        </main>
+    )
+}
+
+export default SignInForm
