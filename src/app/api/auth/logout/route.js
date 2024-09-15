9e45dc402071c6c95ad16357a803c7cef0c185f5
hillary-254 <30282089+larykip@users.noreply.github.com>
2024-09-15 19:59:41 +0300

    Fixed cookies session issue & created a logout API

---
+++ src/app/api/auth/logout/route.js
@@ -0,0 +1,8 @@
+// /api/auth/logout.js
+import { NextResponse } from 'next/server';
+
+export async function POST() {
+  const response = NextResponse.json({ message: 'Logged out' });
+  response.cookies.set('token', '', { maxAge: 0, path: '/' });  // Clear the cookie
+  return response;
+}
