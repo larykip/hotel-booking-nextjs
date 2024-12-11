b3de7b154a5196d9626605abee34b19238373f38
mergik <122933591+mergik@users.noreply.github.com>
2024-12-11 19:56:02 +0300

    feat: Creates the rest of the dashboard routes

---
+++ src/app/dashboard/rooms/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoomsPage = () => {
+  return (
+    <section className='bg-yellow-200 p-4 h-screen'>Rooms Page</section>
+  )
+}
+
+export default RoomsPage
---
+++ src/app/dashboard/settings/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SettingsPage = () => {
+  return (
+    <section className='bg-indigo-200 p-4 h-screen'>Settings Page</section>
+  )
+}
+
+export default SettingsPage
