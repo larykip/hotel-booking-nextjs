a9241070c083fac26c6b922789ac772ce258f096
stevecmd <murimi101@gmail.com>
2025-01-29 22:16:15 +0300

    feat: Updated user schema fields for future reference

--- src/models/user.js
+++ src/models/user.js
@@ -26,6 +26,23 @@ const userSchema = new Schema({
         required: true
     },
     role: { type: String, enum: ['guest', 'admin'], default: 'guest' },
+    // currentAddress: {
+    //     type: String,
+    //     required: true
+    // },
+    // contactNumber: {
+    //     type: String,
+    //     required: true
+    // },
+    // idType: {
+    //     type: { String, enum: ['National-ID', 'Passport'], default: 'National-ID' },
+    //     // required: true
+    // },
+    // idNumber: {
+    //     type: String,
+    //     required: true,
+    //     unique: true
+    // },
 }, { timestamps: true });
 
 const User = mongoose.models.User || mongoose.model('User', userSchema);
