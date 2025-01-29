ae53c488fe77f76b99b2b3cd2e2bf26a113730be
stevecmd <murimi101@gmail.com>
2025-01-29 14:09:13 +0300

    refactor: Remove commented-out code from AppSidebar component

--- src/components/appsidebar/AppSidebar.jsx
+++ src/components/appsidebar/AppSidebar.jsx
@@ -93,25 +93,3 @@ export function AppSidebar() {
 		</Sidebar>
 	);
 }
-
-// CUSTOM SIDEBAR DESIGN
-
-// import LogoutButton from './LogoutButton'
-// import AccountToggle from './AccountToggle'
-// import Search from './Search'
-
-// export const Sidebar = () => {
-//   return (
-//     <section className='p-4 border-r border-r-stone-400 border-opacity-25 bg-neutral-200'>
-
-//         <div className='overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]'>
-//           <AccountToggle />
-//           <Search />
-//         </div>
-
-//         {/* TODO: Logout button */}
-//         <LogoutButton />
-
-//     </section>
-//   )
-// }
